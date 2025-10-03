9345b5a520b3464f771bebdcae503adbb146bd16
Alexander Nagnitchenko <nagnit4enko@mail.ru>
2025-10-04 00:53:00 +0500

    Update buffer.go

--- core/inmem/buffer.go
+++ core/inmem/buffer.go
@@ -7,8 +7,8 @@ import (
 	"sync"
 	"sync/atomic"
 
-	"github.com/vkcom/kittenhouse/core/kittenerror"
-	"github.com/vkcom/kittenhouse/core/persist"
+	"github.com/nagnit4enko/kittenhouse/core/kittenerror"
+	"github.com/nagnit4enko/kittenhouse/core/persist"
 )
 
 type (
